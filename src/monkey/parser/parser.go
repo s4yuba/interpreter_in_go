7c074f87e4d770a65212467b218b3586d3ddf03c
sayuba <sho.yuba48@gmail.com>
2024-12-23 11:23:27 +0900

    2.5 return parser

--- src/monkey/parser/parser.go
+++ src/monkey/parser/parser.go
@@ -62,6 +62,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -82,6 +84,20 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 
 	// TODO: we're skipping the expressions until we
 	// encouter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
+	// TODO: we're skipping the expresssions until we
+	// encounter a semicolon
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
