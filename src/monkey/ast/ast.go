47a830764ebc9426547ffc4ec5c2e1809d77f2c5
sayuba <sho.yuba48@gmail.com>
2024-12-25 22:20:15 +0900

    2.6.7 integer

--- src/monkey/ast/ast.go
+++ src/monkey/ast/ast.go
@@ -110,3 +110,12 @@ type Expression interface {
 	Node
 	expressionNode()
 }
+
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+func (il *IntegerLiteral) expressionNode()      {}
+func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
+func (il *IntegerLiteral) String() string       { return il.Token.Literal }
